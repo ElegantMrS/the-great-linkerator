f519cf7d8fe729817fbbedb0572ade3b7cff4062
ElegantMrS <josephjumber@gmail.com>
2021-04-18 21:16:24 -0400

    hopefully final

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 import LinkCard from './LinkCard';
-import { getAllLinks } from '../api';
 
 const useStyles = makeStyles({
     root: {
@@ -14,20 +13,9 @@ const useStyles = makeStyles({
     },
 });
 
-const Links = ({links, setLinks}) => {
+const Links = ({links}) => {
 
     const classes = useStyles();
-
-    useEffect(() => {
-        try {
-            Promise.all([getAllLinks()]).then(([data]) => {
-                setLinks(data);
-                console.log(data);
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }, []);
 
     return (
         <div className={classes.root}>
